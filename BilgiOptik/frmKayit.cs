6b2266585b14b4516e65f65fcc5b621233b30f6f
sefaCokkaraman <sefacokkaraman@hotmail.com>
2021-06-07 13:18:28 +0300

    Kayıt Hatası Giderildi

--- BilgiOptik/frmKayit.cs
+++ BilgiOptik/frmKayit.cs
@@ -6,6 +6,8 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +22,13 @@ namespace BilgiOptik
         Musteri m = new Musteri();
         Tedarikci t = new Tedarikci();
 
+        string hsifre, hkullaniciAdi, mail, sifre, mesaj;
+        public static string to;
+
+     
+        BilgiOptikFirma bof = new BilgiOptikFirma();
+
+
         public frmKayit()
         {
             InitializeComponent();
@@ -27,7 +36,7 @@ namespace BilgiOptik
 
         private void frmKayit_Load(object sender, EventArgs e)
         {
-
+            bof = db.BilgiOptikFirma.FirstOrDefault();
             txtVergiNo.Visible = false;
             if (Program.k.yetkiID == 2)
             {
@@ -40,6 +49,27 @@ namespace BilgiOptik
             {
                 rbMusteri.Visible = true;
                 rbMusteri.Checked = false;
+            }
+        }
+        private void textboxTemizleme()
+        {
+            foreach (var c in this.Controls)
+            {
+                if (c is TextBox)
+                {
+
+                    ((TextBox)c).Text = "";
+
+
+                }
+                else if (c is RichTextBox)
+                {
+                    ((RichTextBox)c).Text = "";
+                }
+                else if (c is MaskedTextBox)
+                {
+                    ((MaskedTextBox)c).Text = "(   ) -";
+                }
             }
         }
 
@@ -101,8 +131,47 @@ namespace BilgiOptik
             //PERSONEL
             else if (rbPersonel.Checked == true && rbMusteri.Checked == false && rbTedarikçi.Checked == false)
             {
-                frmGirisKayit frmGirisKayit = new frmGirisKayit();
-                frmGirisKayit.btnKayit_Click(sender, e);
+                 if (txtAd.Text == "" || txtSoyad.Text == "" || txtEposta.Text == "" || mtbTelefon.Text == "(   )    -" || txtTCKimlik.Text == "" || rtbAdres.Text == "")
+            {
+                MessageBox.Show("Boş Geçilemez!");
+            }
+            else
+            {
+                k.yetkiID = 2;
+                k.adi = txtAd.Text;
+                k.soyadi = txtSoyad.Text;
+                k.eposta = txtEposta.Text;
+                k.telefon = mtbTelefon.Text;
+                k.TCKimlikNo = txtTCKimlik.Text;
+                k.adres = rtbAdres.Text;
+                k.durum = true;
+                Random rand = new Random();
+                hkullaniciAdi = (rand.Next(999999)).ToString();
+                hsifre = (rand.Next(999999)).ToString();
+                k.kullaniciAdi = hkullaniciAdi;
+                k.sifre = hsifre;
+                MailMessage message = new MailMessage();
+                to = (k.eposta).ToString();
+                mail = bof.email;
+                sifre = bof.sifre;
+                mesaj = "Merhaba" + k.adi + " " + k.soyadi + "," + "\n \n Hesap Kullanıcı Adınız :" + hkullaniciAdi + "\n Hesap Şifreniz: " + hsifre;
+                message.To.Add(to);
+                message.From = new MailAddress(mail);
+                message.Body = mesaj;
+                message.Subject = "Bilgi Optik Kayıt Talebi";
+                SmtpClient smtp = new SmtpClient("smtp.gmail.com");
+                smtp.EnableSsl = true;
+                smtp.Port = 587;
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.Credentials = new NetworkCredential(mail, sifre);
+                smtp.Send(message);
+                MessageBox.Show("Personel Kayıtı Başarıyla Gerçekleşmiştir.\nKullanıcı Adınız ve Şifreniz Mail Hesabınıza Yollanmıştır", "Personel Kayıt", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show("Ad-Soyad: " + k.adi + " " + k.soyadi + "\nEposta: " + k.eposta + "\nTelefon: " + k.telefon + "\nT.C Kimlik No: " + k.TCKimlikNo
+                + "\nAdres: " + k.adres);
+                db.Kullanici.Add(k);
+                db.SaveChanges();
+            }
+            textboxTemizleme();
             }
 
             //TEDARİKÇİ
